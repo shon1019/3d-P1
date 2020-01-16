4c045c9ab613def5a52b26e2ed442e7f608552dd
范茗翔 <shion10191@gmail.com>
2020-01-16 19:16:03 +0800

    tmp

--- bvhReader.py
+++ bvhReader.py
@@ -47,7 +47,7 @@ class concat(bpy.types.Operator):
         obj.animation_data.nla_tracks.new()
         obj.animation_data.nla_tracks.new()
         #------------------------------------------------------------------
-        obj.animation_data.nla_tracks['NlaTrack'].strips.new("dance", 1.0, bpy.data.actions['dance'])
+        obj.animation_data.nla_tracks['NlaTrack'].strips.new("dance", 1.0, bpy.data.actions['dance1'])
         obj.animation_data.nla_tracks['NlaTrack.001'].strips.new("dance", 1.0, bpy.data.actions['dance'])
         return {'FINISHED'}
 
@@ -57,13 +57,18 @@ class cameraTrack(bpy.types.Operator):
 
     def execute(self, context):
         camera = bpy.data.objects['Camera']
+
+        camera.select_set(True)
+        context.view_layer.objects.active = bvhUtils.curveOB
+        bvhUtils.curveOB.select_set(True)
+        bpy.ops.object.parent_set(type='FOLLOW') #follow path
+
         targetobj = context.object
         ttc = camera.constraints.new(type='TRACK_TO')
         ttc.target = targetobj
         ttc.track_axis = 'TRACK_NEGATIVE_Z'
         ttc.up_axis = 'UP_Y'
 
-        #trackLine----------------------------
         return {'FINISHED'}
 
 
@@ -92,6 +97,7 @@ class bvhReader(bpy.types.Operator):
         aniTime = 0
 
         bvhUtils.firstLoad = True
+        print("*------------------------")
         for i in range (1, context.scene.frame_end):
             aniTime += 1
             sc.frame_set(i)
@@ -105,6 +111,14 @@ class bvhReader(bpy.types.Operator):
             elif i == 1:
                 bvhUtils.HipLocations.append(tmpLocation)
             else:
+                print(i)
+                print("*------------------------")
+                print(tmpLocation[0] )
+                print(bvhUtils.HipLocations[i-2][0])
+                print(tmpLocation[1])
+                print(bvhUtils.HipLocations[i-2][1])
+                print(tmpLocation[2])
+                print(bvhUtils.HipLocations[i-2][2])
                 break
 
         points = SolveLeastSquare.leastsq(bvhUtils.HipLocations)
@@ -143,13 +157,18 @@ class bvhReader(bpy.types.Operator):
 
         #remove all root translate
         bvh = context.object
-        for i in range(1, aniTime):
+        obj.rotation_euler[2] = 3.14
+        print(aniTime)
+        for i in range(0, aniTime):
+            
             sc.frame_set(i)
-            bvh.pose.bones['Hips'].keyframe_delete(data_path="location",frame = i)
             obj.pose.bones['Hips'].location[0] = 0
             obj.pose.bones['Hips'].location[1] = 0
+            obj.pose.bones['Hips'].location[2] = 0
+            bvh.pose.bones['Hips'].keyframe_delete(data_path="location",frame = i)
+            
         
-
+        bpy.ops.object.select_all(action='DESELECT')
         return {'FINISHED'}
 
 
